7015022aac04ea3bf95324c1adf9637900fe691d
Hafs Ibrahim <hafs.darwish@gmail.com>
2019-10-15 03:49:03 +0200

    BD chocolate

---
+++ HackerRank/Algorithms/Algorithms/Implementation/Birthday Chocolate/solution.js
@@ -0,0 +1,60 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function (inputStdin) {
+  inputString += inputStdin;
+});
+
+process.stdin.on('end', function () {
+  inputString = inputString.split('\n');
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+// Complete the birthday function below.
+function birthday(s, d, m) {
+  const sum = new Array(101);
+  let count = 0;
+  sum[0] = 0;
+  for (let i = 0; i < s.length; i++) {
+    sum[i + 1] = sum[i] + s[i];
+  }
+  for (let i = 0; i <= s.length - m; i++) {
+    if (sum[i + m] - sum[i] === d) {
+      count++;
+    }
+  }
+  return count;
+
+}
+
+function main() {
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+  const n = parseInt(readLine().trim(), 10);
+
+  const s = readLine().replace(/\s+$/g, '').split(' ').map(sTemp => parseInt(sTemp, 10));
+
+  const dm = readLine().replace(/\s+$/g, '').split(' ');
+
+  const d = parseInt(dm[0], 10);
+
+  const m = parseInt(dm[1], 10);
+
+  const result = birthday(s, d, m);
+
+  ws.write(result + '\n');
+
+  ws.end();
+}
